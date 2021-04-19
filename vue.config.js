047a9fb37b8ef4d64bb8d7fa10133bf99483285c
AndreyKudinov63 <64890665+AndreyKudinov63@users.noreply.github.com>
2021-04-19 22:21:39 +0400

    правки для gh pages

--- vue.config.js
+++ vue.config.js
@@ -4,30 +4,30 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/sweethome/'
     : '/',
-  module: {
-    rules: [
-      {
-        test: /\.s(c|a)ss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            // Requires sass-loader@^7.0.0
-            options: {
-              implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
-            // Requires >= sass-loader@^8.0.0
-            options: {
-              implementation: require('sass'),
-              sassOptions: {
-                indentedSyntax: true // optional
-              },
-            },
-          },
-        ],
-      },
-    ],
-  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.s(c|a)ss$/,
+  //       use: [
+  //         'vue-style-loader',
+  //         'css-loader',
+  //         {
+  //           loader: 'sass-loader',
+  //           // Requires sass-loader@^7.0.0
+  //           options: {
+  //             implementation: require('sass'),
+  //             indentedSyntax: true // optional
+  //           },
+  //           // Requires >= sass-loader@^8.0.0
+  //           options: {
+  //             implementation: require('sass'),
+  //             sassOptions: {
+  //               indentedSyntax: true // optional
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // }
 }
